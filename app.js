881306363b9cbbefb07eb0618e79a9cd90a5c9f6
kozmaatilar <38038614+kozmaatilar@users.noreply.github.com>
2024-04-25 20:42:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // The main web app file.
 
 // Server port
-const PORT = 80;
+const PORT = 80
 
 // Imports
-var express = require('express');
-var http = require('http');
-var createError = require('http-errors');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var session = require('express-session');
-var sessionStore = require('connect-sqlite3')(session);
+var express = require('express')
+var http = require('http')
+var createError = require('http-errors')
+var bodyParser = require('body-parser')
+var passport = require('passport')
+var session = require('express-session')
+var sessionStore = require('connect-sqlite3')(session)
 var path = require('path');
 var csrf = require('csurf');
 
